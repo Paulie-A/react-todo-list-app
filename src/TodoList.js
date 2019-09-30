dda807b1e6908e93c44f0510377e1f83235e4b04
Paul August <paul.augustp@gmail.com>
2019-10-01 00:04:11 +0100

    css styling added

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import NewTodoForm from "./NewTodoForm";
 import Todo from  "./Todo"
+import "./TodoList.css";
+
 
 class TodoList extends Component {
     constructor(props) {
@@ -54,10 +56,10 @@ class TodoList extends Component {
                 />
             });
             return (
-            <div>
-                <h1>Todo List!</h1>
-                <NewTodoForm createTodo={this.create} />
+            <div className="TodoList">
+                <h1>Todo List! <span>React Todo List App</span></h1>
                 <ul>{todos}</ul>
+                <NewTodoForm createTodo={this.create} />
             </div>
         );
     }
